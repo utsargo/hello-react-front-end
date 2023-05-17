7ed80f39098343cbd84e1c5cad64c7546f9b1182
Utsargo Roy <royutsargo@gmail.com>
2023-05-17 22:11:22 +0600

    Complete frontend

---
+++ src/store/greetingsSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  greetings: [],
+  loading: false,
+};
+
+const greetingsSlice = createSlice({
+  name: 'greetings',
+  initialState,
+  reducers: {
+    setGreetings: (state, action) => ({
+      ...state,
+      greetings: action.payload,
+    }),
+  },
+});
+
+export const { setGreetings } = greetingsSlice.actions;
+
+export default greetingsSlice.reducer;
+
+const fetchGreetings = () => async (dispatch) => {
+  try {
+    const res = await fetch('//localhost:3000/api/v1/greetings');
+    const data = await res.json();
+    dispatch(setGreetings(data));
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+  }
+};
+
+export { fetchGreetings };
