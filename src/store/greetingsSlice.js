5f25738ddb0165f28c8f97af99219786e9398731
Utsargo Roy <royutsargo@gmail.com>
2023-05-18 01:31:28 +0600

    Remove console.log

--- src/store/greetingsSlice.js
+++ src/store/greetingsSlice.js
@@ -20,14 +20,14 @@ export const { setGreetings } = greetingsSlice.actions;
 
 export default greetingsSlice.reducer;
 
+
 const fetchGreetings = () => async (dispatch) => {
   try {
     const res = await fetch('//localhost:3000/api/v1/greetings');
     const data = await res.json();
     dispatch(setGreetings(data));
   } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log(error);
+    throw new Error(error);
   }
 };
 
